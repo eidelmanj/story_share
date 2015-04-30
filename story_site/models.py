971530002e249ea63a90082930cc0dba69dc5f86
eidelmanj <jonathan.eidelman@gmail.com>
2015-04-30 17:36:02 -0400

    Added uploading arbitrary files

--- story_site/models.py
+++ story_site/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+### Useless model that I can't seem to get rid of without breaking things
+class AudioFile(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=1000)
+    owner = models.ForeignKey(User, unique=False)
+    filePath = models.CharField(max_length=500)
+
+
+class OwnedAudioFile(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=1000)
+    owner = models.ForeignKey(User, unique=False)
+    filePath = models.CharField(max_length=500)
