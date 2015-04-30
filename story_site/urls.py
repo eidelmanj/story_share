f7c9c9e3e2efe1d8c75da67133618778bc8e1bf4
eidelmanj <jonathan.eidelman@gmail.com>
2015-04-30 17:36:16 -0400

    Added uploading arbitrary files

--- story_site/urls.py
+++ story_site/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^logout/', views.logout_page, name='logout_page'),
     url(r'^register/', views.register_page, name='register_page'),
     url(r'^generate_user/', views.generate_user, name='generate_user'),
-    url(r'^authenticate/', views.authentication_page, name='authentication_page')
+    url(r'^authenticate/', views.authentication_page, name='authentication_page'),
+    url(r'^upload/', views.upload_file, name='upload_file'),
+    url(r'^user_page/', views.user_page, name='user_page'),
 ]
--- story_site/views.py
+++ story_site/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, render_to_response
 from django.template import RequestContext, loader
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from .forms import UploadAudioFileForm
+from files import handle_uploaded_file
+from django.core.context_processors import csrf
 
 # Create your views here.
 def index(request):
@@ -21,18 +24,18 @@ def authentication_page(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                return redirect('/story_site/')
-            return redirect('/story_site/login_error.html')
+                return redirect('index')
+            return redirect('login_error')
         else:
-            return redirect('/story_site/login_error.html')
+            return redirect('login_error')
 
     else:
-        return redirect('/story_site/login_error.html')
+        return redirect('login_error')
 
 
 def logout_page(request):
     logout(request)
-    return redirect('/story_site/')
+    return redirect('index')
 
 
 def register_page(request):
@@ -45,6 +48,12 @@ def register_page(request):
     request.session.modified = True
     
     return render(request, 'story_site/register.html', {"userAlreadyTaken" : userAlreadyTaken})
+
+def user_page(request):
+    form = UploadAudioFileForm()
+    c = { "form" : form }
+    c.update(csrf(request))
+    return render(request, 'story_site/userhome.html', c)
 
 
 def generate_user(request):
@@ -62,13 +71,33 @@ def generate_user(request):
                 
             if user:
                 print user.username
-                return redirect('/story_site/')
+                return redirect('index')
             else:
-                return redirect('/story_site/register/')
+                return redirect('register_page')
 
         else:
-            return redirect('/story_site/register_error/')
+            return redirect('register_error')
 
     else:
-        return redirect('/story_site/register_error/')
+        return redirect('register_error')
             
+
+
+def upload_file(request):
+
+    if not request.user.is_authenticated:
+        return redirect('login_page')
+
+    
+    
+    if request.method=="POST":
+        form = UploadAudioFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # print str(request.FILES.keys())
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            
+            handle_uploaded_file(request.FILES['file'], request.user, title, description )
+            return redirect('user_page')
+
+    return redirect('index')
